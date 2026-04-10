30e3394455725b63ac28175586538f79f21c8b05
James Kho <154097220+atxjkho@users.noreply.github.com>
2026-04-10 15:51:44 +1000

    Add deploy-source-nat-config.sh for iptables NAT setup
    This script sets up NAT rules using iptables to translate source IP addresses to a specified NATed IP address. It also enables IP forwarding and persists the iptables rules.

---
+++ scripts/bash/deploy-source-nat-config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+# Check if all parameters are provided
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <NATed_IP_ADDRESS> <SOURCE_IP_CIDR> <TCP_PORT> <DESTINATION_IP_CIDR>"
+    exit 1
+fi
+
+# Assign the parameters to variables
+NATed_IP_ADDRESS=$1
+SOURCE_IP_CIDR=($2)
+TCP_PORT=$3
+DESTINATION_IP_CIDR=($4)
+
+#Create the directory:
+sudo mkdir -p /etc/iptables
+
+
+#Configure iptables: Use iptables to set up the NAT rule to translate the source IP to the defined NATed IP address.
+
+# Enable IP Forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+
+# Set up NAT for traffic from 10.253.134.16/28 to 10.125.80.50:80
+for DEST in "${DESTINATION_IP_CIDR[@]}"; do
+    for SOURCE in "${SOURCE_IP_CIDR[@]}"; do
+        sudo iptables -t nat -A PREROUTING -s $SOURCE -d $DEST -p tcp --dport $TCP_PORT -j DNAT --to-destination $NATed_IP_ADDRESS:$TCP_PORT
+    done
+done
+    
+
+# Add a rule to forward traffic to 10.125.80.0/24
+for DEST in "${DESTINATION_IP_CIDR[@]}"; do
+    sudo iptables -A FORWARD -d $DEST -j ACCEPT
+    sudo iptables -A FORWARD -s $DEST -j ACCEPT
+done
+
+
+#Persist iptables Rules:
+sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+
