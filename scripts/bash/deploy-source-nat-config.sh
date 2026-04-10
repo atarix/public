f58d43eca35d5e33895491118077fe57fe647e66
James Kho <154097220+atxjkho@users.noreply.github.com>
2026-04-10 16:18:42 +1000

    Fix parameter usage and assignment in deploy script
    Updated usage message and parameter assignment in deploy-source-nat-config.sh

--- scripts/bash/deploy-source-nat-config.sh
+++ scripts/bash/deploy-source-nat-config.sh
@@ -4,15 +4,14 @@ set -e
 
 # Check if all parameters are provided
 if [ "$#" -ne 4 ]; then
-    echo "Usage: $0 <NATed_IP_ADDRESS> <SOURCE_IP_CIDR> <TCP_PORT> <DESTINATION_IP_CIDR>"
+    echo "Usage: $0 <NATed_IP_ADDRESS> <SOURCE_IP_CIDR> <DESTINATION_IP_CIDR>"
     exit 1
 fi
 
 # Assign the parameters to variables
 NATed_IP_ADDRESS=$1
 SOURCE_IP_CIDR=($2)
-TCP_PORT=$3
-DESTINATION_IP_CIDR=($4)
+DESTINATION_IP_CIDR=($3)
 
 #Create the directory:
 sudo mkdir -p /etc/iptables
@@ -40,3 +39,4 @@ done
 
 #Persist iptables Rules:
 sudo sh -c "iptables-save > /etc/iptables/rules.v4"
+
