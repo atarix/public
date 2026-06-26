9be1598dce15ebce636fb078e76b0701e7bac11d
bharatperi-atx <bharat.peri@atarix.com.au>
2026-06-26 10:41:21 +1000

    Update Azure DevOps agent version to 4.274.1

--- scripts/bash/deploy-devopsagent.sh
+++ scripts/bash/deploy-devopsagent.sh
@@ -31,7 +31,7 @@ ORG_URL=$1
 POOL_NAME=$2
 PAT_TOKEN=$3
 AGENT_NAME=$4
-AGENT_VERSION_URL="https://download.agent.dev.azure.com/agent/4.258.1/vsts-agent-linux-x64-4.258.1.tar.gz"
+AGENT_VERSION_URL="https://download.agent.dev.azure.com/agent/4.274.1/vsts-agent-linux-x64-4.274.1.tar.gz"
 INSTALL_DIR="/opt/azure/devops/agent"
 
 echo "Starting Azure DevOps agent setup..."
